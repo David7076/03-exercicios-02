074f4aec819dc9d148604e739f3c328c5a56c290
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-27 22:45:55 -0300

    Exercicio feito

---
+++ src/exercicio02/Ponto.java
@@ -0,0 +1,31 @@
+package exercicio02;
+
+public class Ponto {
+    int x;
+    int y;
+
+    public double calcularDistancia(Ponto p) {
+        double distancia;
+        distancia = Math.sqrt(Math.pow(x - p.x, 2)) + Math.pow(y - p.y, 2);
+        return distancia;
+    }
+
+    public double calcularDistanciaAteOrigem() {
+        Ponto origem = new Ponto();
+        return calcularDistancia(origem);
+    }
+
+    public String formatarPonto() {
+        return "(" + x+ " , " + y + ")";
+    }
+
+    // método para retornar o ponto mais proximo da origem
+    public Ponto maisProximadaOrigem(Ponto p1, Ponto p2) {
+        double dp1 = p1.calcularDistanciaAteOrigem();
+        double dp2 = p2.calcularDistanciaAteOrigem();
+        if(dp1 < dp2) {
+            return p1;
+        }
+        return p2;
+    }
+}
